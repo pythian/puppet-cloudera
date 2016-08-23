92ac08ff1c3e6534c21dd26b24583863c517f576
Thiago Carvalho <carvalho@pythian.com>
2016-08-23 21:46:50 +0000

    adds parcels manifests

---
+++ manifests/cluster/parcels/download.pp
@@ -0,0 +1,21 @@
+define cloudera::cluster::parcels::download (
+  $cdh_metadata_dir  = $cloudera::params::cdh_metadata_dir,
+  $cdh_cluster_name  = $cloudera::params::cdh_cluster_name,
+  $parcels_product   = $title,
+  $parcels_version   = $cloudera::params::parcels_version,
+  $cm_api_host       = $cloudera::params::cm_api_host,
+  $cm_api_port       = $cloudera::params::cm_api_port,
+  $cm_api_user       = $cloudera::params::cm_api_user,
+  $cm_api_password   = $cloudera::params::cm_api_password
+) {
+
+  exec { "parcels-download-$parcels_product":
+    command => "/usr/bin/curl -u $cloudera::params::cm_api_user:$cloudera::params::cm_api_password -XPOST \"http://$cm_api_host:$cm_api_port/api/v13/clusters/$cdh_cluster_name/parcels/products/$parcels_product/versions/$parcels_version/commands/startDownload\" > $cdh_metadata_dir/parcels-download-$parcels_product.json.output",
+    cwd     => "/tmp",
+    creates => "$cdh_metadata_dir/parcels-download-$parcels_product.json.output",
+    tries   => 3,
+    try_sleep => 60
+  }
+
+}
+

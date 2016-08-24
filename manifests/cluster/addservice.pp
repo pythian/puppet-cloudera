39277f6fc024a7e4697ed7b54f7d0366d2db2648
Thiago Nache B. de Carvalho <carvalho@pythian.com>
2016-08-24 20:52:40 -0300

    changes api version and renames variable

--- manifests/cluster/addservice.pp
+++ manifests/cluster/addservice.pp
@@ -34,36 +34,36 @@ define cloudera::cluster::addservice (
   $cm_api_port       = $cloudera::params::cm_api_port,
   $cm_api_user       = $cloudera::params::cm_api_user,
   $cm_api_password   = $cloudera::params::cm_api_password,
-  $hadoop_service_name = $title,
+  $cdh_service_name = $title,
   $cdh_service_roles = $cloudera::params::cdh_service_roles
 ) {
 
-  file { "$hadoop_service_name.json":
+  file { "$cdh_service_name.json":
     ensure  => $file_ensure,
-    path    => "/tmp/$hadoop_service_name.json",
+    path    => "/tmp/$cdh_service_name.json",
     content => template("${module_name}/service.json.erb")
   }
 
-  exec { "add service $hadoop_service_name":
-    command => "/usr/bin/curl -H 'Content-Type: application/json' -u $cloudera::params::cm_api_user:$cloudera::params::cm_api_password -XPOST \"http://$cm_api_host:$cm_api_port/api/v1/clusters/$cdh_cluster_name/services\" -d @$hadoop_service_name.json > $cdh_metadata_dir/$hadoop_service_name.json.output",
+  exec { "add service $cdh_service_name":
+    command => "/usr/bin/curl -H 'Content-Type: application/json' -u $cloudera::params::cm_api_user:$cloudera::params::cm_api_password -XPOST \"http://$cm_api_host:$cm_api_port/api/v13/clusters/$cdh_cluster_name/services\" -d @$cdh_service_name.json > $cdh_metadata_dir/$cdh_service_name.json.output",
     cwd     => "/tmp",
-    creates => "$cdh_metadata_dir/$hadoop_service_name.json.output",
-    require => File["$hadoop_service_name.json"],
+    creates => "$cdh_metadata_dir/$cdh_service_name.json.output",
+    require => File["$cdh_service_name.json"],
     tries   => 3,
     try_sleep => 60
   }
 
-  file { "$hadoop_service_name-roles.json":
+  file { "$cdh_service_name-roles.json":
     ensure  => $file_ensure,
-    path    => "/tmp/$hadoop_service_name-roles.json",
+    path    => "/tmp/$cdh_service_name-roles.json",
     content => template("${module_name}/roles.json.erb")
   }
 
-  exec { "add role for service $hadoop_service_name":
-    command => "/usr/bin/curl -H 'Content-Type: application/json' -u $cloudera::params::cm_api_user:$cloudera::params::cm_api_password -XPOST \"http://$cm_api_host:$cm_api_port/api/v1/clusters/$cdh_cluster_name/services/$hadoop_service_name/roles\" -d @$hadoop_service_name-roles.json > $cdh_metadata_dir/$hadoop_service_name-roles.json.output",
+  exec { "add role for service $cdh_service_name":
+    command => "/usr/bin/curl -H 'Content-Type: application/json' -u $cloudera::params::cm_api_user:$cloudera::params::cm_api_password -XPOST \"http://$cm_api_host:$cm_api_port/api/v13/clusters/$cdh_cluster_name/services/$cdh_service_name/roles\" -d @$cdh_service_name-roles.json > $cdh_metadata_dir/$cdh_service_name-roles.json.output",
     cwd     => "/tmp",
-    creates => "$cdh_metadata_dir/$hadoop_service_name-roles.json.output",
-    require => [File["$hadoop_service_name-roles.json"],Exec["add service $hadoop_service_name"]],
+    creates => "$cdh_metadata_dir/$cdh_service_name-roles.json.output",
+    require => [File["$cdh_service_name-roles.json"],Exec["add service $cdh_service_name"]],
     tries   => 3,
     try_sleep => 60
   }

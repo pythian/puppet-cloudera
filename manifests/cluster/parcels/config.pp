1563f42a6d0845908d0842ea99d1c3b018ab6e36
root <root@ip-172-31-45-62.us-west-2.compute.internal>
2016-08-24 00:20:19 +0000

    adds manifest to configure parcels

---
+++ manifests/cluster/parcels/config.pp
@@ -0,0 +1,26 @@
+define cloudera::cluster::parcels::config (
+  $cdh_metadata_dir  = $cloudera::params::cdh_metadata_dir,
+  $cm_api_host       = $cloudera::params::cm_api_host,
+  $cm_api_port       = $cloudera::params::cm_api_port,
+  $cm_api_user       = $cloudera::params::cm_api_user,
+  $cm_api_password   = $cloudera::params::cm_api_password,
+  $items_config      = $cloudera::params::items_config,
+  $parcels_product   = $title
+) {
+
+  file { "parcels-config.json":
+    ensure  => $file_ensure,
+    path    => "/tmp/parcels-config.json",
+    content => template("${module_name}/config.json.erb")
+  }
+
+  exec { "parcels-config-$parcels_product":
+    command => "/usr/bin/curl -H 'Content-Type: application/json' -u $cloudera::params::cm_api_user:$cloudera::params::cm_api_password -XPUT \"http://$cm_api_host:$cm_api_port/api/v13/cm/config\" -d @parcels-config.json > $cdh_metadata_dir/parcels-config-$parcels_product.json.output",
+    cwd     => "/tmp",
+    creates => "$cdh_metadata_dir/parcels-config-$parcels_product.json.output",
+    tries   => 3,
+    try_sleep => 60,
+    require => File["parcels-config.json"]
+  }
+
+}

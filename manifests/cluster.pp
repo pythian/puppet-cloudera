e06e452c2a50e76b23ea2c80ff6e6b9680d818c2
Thiago Nache B. de Carvalho <carvalho@pythian.com>
2016-08-25 20:42:29 -0300

    changing approach again

--- manifests/cluster.pp
+++ manifests/cluster.pp
@@ -1,30 +1,8 @@
 # == Class: cloudera::cluster
 class cloudera::cluster (
   $cdh_metadata_dir = $cloudera::params::cdh_metadata_dir,
-  $cdh_cluster_name = $cloudera::params::cdh_cluster_name,
-  $cm_api_host      = $cloudera::params::cm_api_host,
-  $cm_api_port      = $cloudera::params::cm_api_port,
-  $install_cmserver = $cloudera::params::install_cmserver,
 ) inherits cloudera::params {
   file { "$cdh_metadata_dir":
     ensure => 'directory'
-  }
-  if $install_cmserver == true {
-    class { '::cloudera':
-      cm_server_host => $cm_api_host,
-      install_cmserver => $install_cmserver,
-      use_parcels => true
-    }
-
-  } else {
-    class { '::cloudera':
-      cm_server_host => $cm_api_host,
-      use_parcels => true
-    }
-  }
-  class { '::cloudera::cluster::addhost':
-    cdh_cluster_name => $cdh_cluster_name,
-    cm_api_host => $cm_api_host,
-    require => Class['::cloudera']
   }
 }

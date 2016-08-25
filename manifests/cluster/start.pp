c059e822d1201b95af7ebc70c94fb5c94f1647aa
Thiago Nache B. de Carvalho <carvalho@pythian.com>
2016-08-25 10:34:58 -0300

    it should be a class not a function

--- manifests/cluster/start.pp
+++ manifests/cluster/start.pp
@@ -27,16 +27,16 @@
 #
 #
 
-define cloudera::cluster::start (
+Class cloudera::cluster::start (
   $cdh_metadata_dir  = $cloudera::params::cdh_metadata_dir,
   $cdh_cluster_name  = $cloudera::params::cdh_cluster_name,
   $cm_api_host       = $cloudera::params::cm_api_host,
   $cm_api_port       = $cloudera::params::cm_api_port,
   $cm_api_user       = $cloudera::params::cm_api_user,
   $cm_api_password   = $cloudera::params::cm_api_password
-) {
+) inherits cloudera::params {
 
-  exec { "start service $cdh_cluster_service":
+  exec { "start cluster $cdh_cluster_name":
     command => "/usr/bin/curl -H 'Content-Type: application/json' -u $cloudera::params::cm_api_user:$cloudera::params::cm_api_password -XPOST \"http://$cm_api_host:$cm_api_port/api/v13/clusters/$cdh_cluster_name/firstRun\" > $cdh_metadata_dir/cluster-started.lock",
     cwd     => "/tmp",
     creates => "$cdh_metadata_dir/cluster-started.lock",

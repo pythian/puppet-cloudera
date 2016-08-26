e18373278bf6590d5d7139b82b1ae893f30ab91d
Thiago Nache B. de Carvalho <carvalho@pythian.com>
2016-08-26 10:41:44 -0300

    adds addrole function

---
+++ manifests/cluster/addrole.pp
@@ -0,0 +1,55 @@
+# == Class: cloudera::cluster::addrole
+#
+# This class handles installing and configuring the Cloudera Manager Server.
+#
+# === Parameters:
+#
+# [*ensure*]
+#   Ensure if present or absent.
+#   Default: present
+#
+# === Actions:
+#
+#
+# === Requires:
+#
+# === Sample Usage:
+#
+#   cloudera::cluster::addrole{'HBASE':
+#     cm_api_host => $cloudera::params::cm_api_host,
+#     cm_api_port => $cloudera::params::cm_api_port
+#   }
+#
+# === Authors:
+#
+#
+# === Copyright:
+#
+#
+
+define cloudera::cluster::addrole (
+  $cdh_metadata_dir  = $cloudera::params::cdh_metadata_dir,
+  $cdh_cluster_name  = $cloudera::params::cdh_cluster_name,
+  $cm_api_host       = $cloudera::params::cm_api_host,
+  $cm_api_port       = $cloudera::params::cm_api_port,
+  $cm_api_user       = $cloudera::params::cm_api_user,
+  $cm_api_password   = $cloudera::params::cm_api_password,
+  $cdh_service_name = $title,
+  $cdh_service_roles = $cloudera::params::cdh_service_roles
+) {
+
+  file { "$cdh_service_name-roles.json":
+    ensure  => $file_ensure,
+    path    => "/tmp/$cdh_service_name-roles.json",
+    content => template("${module_name}/roles.json.erb")
+  }
+
+  exec { "add role for service $cdh_service_name":
+    command => "/usr/bin/curl -H 'Content-Type: application/json' -u $cloudera::params::cm_api_user:$cloudera::params::cm_api_password -XPOST \"http://$cm_api_host:$cm_api_port/api/v13/clusters/$cdh_cluster_name/services/$cdh_service_name/roles\" -d @$cdh_service_name-roles.json > $cdh_metadata_dir/$cdh_service_name-roles.json.output",
+    cwd     => "/tmp",
+    creates => "$cdh_metadata_dir/$cdh_service_name-roles.json.output",
+    require => [File["$cdh_service_name-roles.json"],Exec["add service $cdh_service_name"]],
+    tries   => 3,
+    try_sleep => 60
+  }
+}

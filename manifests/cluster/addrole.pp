f9ab37fdda931115d2ea883aa8fda29b2a3ad40d
Thiago Nache B. de Carvalho <carvalho@pythian.com>
2016-08-26 10:43:32 -0300

    removes require

--- manifests/cluster/addrole.pp
+++ manifests/cluster/addrole.pp
@@ -48,7 +48,7 @@ define cloudera::cluster::addrole (
     command => "/usr/bin/curl -H 'Content-Type: application/json' -u $cloudera::params::cm_api_user:$cloudera::params::cm_api_password -XPOST \"http://$cm_api_host:$cm_api_port/api/v13/clusters/$cdh_cluster_name/services/$cdh_service_name/roles\" -d @$cdh_service_name-roles.json > $cdh_metadata_dir/$cdh_service_name-roles.json.output",
     cwd     => "/tmp",
     creates => "$cdh_metadata_dir/$cdh_service_name-roles.json.output",
-    require => [File["$cdh_service_name-roles.json"],Exec["add service $cdh_service_name"]],
+    require => File["$cdh_service_name-roles.json"],
     tries   => 3,
     try_sleep => 60
   }

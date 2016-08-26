a6cf7728fd823302c2d9c729b91ebbeeebdb9812
Thiago Carvalho <carvalho@pythian.com>
2016-08-26 16:01:32 +0000

    adds servicenode_1 role abstraction

---
+++ manifests/cluster/roles/servicenode_1.pp
@@ -0,0 +1,42 @@
+class cloudera::cluster::roles::servicenode_1 (
+  $file_ensure       = $cloudera::params::file_ensure,
+  $cdh_metadata_dir  = $cloudera::params::cdh_metadata_dir,
+  $cdh_cluster_name  = $cloudera::params::cdh_cluster_name,
+  $cdh_cluster_ha    = $cloudera::params::cdh_cluster_ha,
+  $cm_api_host       = $cloudera::params::cm_api_host,
+  $cm_api_port       = $cloudera::params::cm_api_port,
+  $cm_api_user       = $cloudera::params::cm_api_user,
+  $cm_api_password   = $cloudera::params::cm_api_password,
+) inherits cloudera::params {
+
+  class { '::cloudera':
+    cm_server_host => $cm_api_host,
+    use_parcels => true
+  }
+  class { '::cloudera::cluster':
+    require => Class['::cloudera']
+  }
+  class { '::cloudera::cluster::addhost':
+    cdh_cluster_name => $cdh_cluster_name,
+    cm_api_host => $cm_api_host,
+    require => Class['::cloudera::cluster']
+  }
+  cloudera::cluster::addrole{'HDFS':
+    cdh_cluster_name => $cdh_cluster_name,
+    cdh_service_roles => ['NAMENODE'],
+    cm_api_host => $cm_api_host,
+    require => Class['::cloudera::cluster::addhost'],
+  }
+  cloudera::cluster::addrole{'HBASE':
+    cdh_cluster_name => $cdh_cluster_name,
+    cdh_service_roles => ['MASTER'],
+    cm_api_host => $cm_api_host,
+    require => Class['::cloudera::cluster::addhost'],
+  }
+  cloudera::cluster::addrole{'ZOOKEEPER':
+    cdh_cluster_name => $cdh_cluster_name,
+    cdh_service_roles => ['SERVER'],
+    cm_api_host => $cm_api_host,
+    require => Class['::cloudera::cluster::addhost'],
+  }
+}

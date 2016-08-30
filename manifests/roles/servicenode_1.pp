0440c2c9525c41615b66a293e23b1f1629efdaa6
Thiago Nache B. de Carvalho <carvalho@pythian.com>
2016-08-30 12:10:07 -0300

    adds cluster ha configuration

--- manifests/roles/servicenode_1.pp
+++ manifests/roles/servicenode_1.pp
@@ -8,7 +8,6 @@ class cloudera::roles::servicenode_1 (
   $cm_api_user       = $cloudera::params::cm_api_user,
   $cm_api_password   = $cloudera::params::cm_api_password,
 ) inherits cloudera::params {
-
   class { '::cloudera':
     cm_server_host => $cm_api_host,
     use_parcels => true
@@ -21,22 +20,43 @@ class cloudera::roles::servicenode_1 (
     cm_api_host => $cm_api_host,
     require => Class['::cloudera::api']
   }
-  cloudera::api::addrole{'HDFS':
-    cdh_cluster_name => $cdh_cluster_name,
-    cdh_service_roles => ['NAMENODE'],
-    cm_api_host => $cm_api_host,
-    require => Class['::cloudera::api::addhost'],
-  }
-  cloudera::api::addrole{'HBASE':
-    cdh_cluster_name => $cdh_cluster_name,
-    cdh_service_roles => ['MASTER'],
-    cm_api_host => $cm_api_host,
-    require => Class['::cloudera::api::addhost'],
-  }
-  cloudera::api::addrole{'ZOOKEEPER':
-    cdh_cluster_name => $cdh_cluster_name,
-    cdh_service_roles => ['SERVER'],
-    cm_api_host => $cm_api_host,
-    require => Class['::cloudera::api::addhost'],
+  if $cdh_cluster_ha == 0 { 
+    cloudera::api::addrole{'HDFS':
+      cdh_cluster_name => $cdh_cluster_name,
+      cdh_service_roles => ['NAMENODE'],
+      cm_api_host => $cm_api_host,
+      require => Class['::cloudera::api::addhost'],
+    }
+    cloudera::api::addrole{'HBASE':
+      cdh_cluster_name => $cdh_cluster_name,
+      cdh_service_roles => ['MASTER'],
+      cm_api_host => $cm_api_host,
+      require => Class['::cloudera::api::addhost'],
+    }
+    cloudera::api::addrole{'ZOOKEEPER':
+      cdh_cluster_name => $cdh_cluster_name,
+      cdh_service_roles => ['SERVER'],
+      cm_api_host => $cm_api_host,
+      require => Class['::cloudera::api::addhost'],
+    }
+  } else {
+    cloudera::api::addrole{'HDFS':
+      cdh_cluster_name => $cdh_cluster_name,
+      cdh_service_roles => ['NAMENODE'],
+      cm_api_host => $cm_api_host,
+      require => Class['::cloudera::api::addhost'],
+    }
+    cloudera::api::addrole{'HBASE':
+      cdh_cluster_name => $cdh_cluster_name,
+      cdh_service_roles => ['MASTER'],
+      cm_api_host => $cm_api_host,
+      require => Class['::cloudera::api::addhost'],
+    }
+    cloudera::api::addrole{'YARN':
+      cdh_cluster_name => $cdh_cluster_name,
+      cdh_service_roles => ['RESOURCEMANAGER'],
+      cm_api_host => $cm_api_host,
+      require => Class['::cloudera::api::addhost'],
+    }
   }
 }

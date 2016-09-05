49cd63cecc471f79499330418e966256f697dd76
Thiago Nache B. de Carvalho <carvalho@pythian.com>
2016-09-05 20:19:48 -0300

    removes start of Management Service from puppet and adds it on script that configures activity monitor to start service after everything configured

--- manifests/api/managementservice.pp
+++ manifests/api/managementservice.pp
@@ -35,7 +35,6 @@ class cloudera::api::managementservice (
   $cm_api_password   = $cloudera::params::cm_api_password,
   $cdh_service_roles = $cloudera::params::cdh_service_roles
 ) {
-
   exec { "add CM MGMT":
     command => "/usr/bin/curl -H 'Content-Type: application/json' -u $cm_api_user:$cm_api_password -XPUT \"http://$cm_api_host:$cm_api_port/api/v13/cm/service\" -d '{}' && touch /var/tmp/CM-MGMT.lock",
     cwd     => "/tmp",
@@ -43,13 +42,11 @@ class cloudera::api::managementservice (
     tries   => 3,
     try_sleep => 60
   }
-
   file { "CM-roles.json":
     ensure  => $file_ensure,
     path    => "/tmp/CM-roles.json",
     content => template("${module_name}/roles.json.erb")
   }
-
   exec { "add role for CM":
     command => "/usr/bin/curl -H 'Content-Type: application/json' -u $cm_api_user:$cm_api_password -XPOST \"http://$cm_api_host:$cm_api_port/api/v13/cm/service/roles\" -d @CM-roles.json && touch /var/tmp/CM-roles.lock",
     cwd     => "/tmp",
@@ -57,14 +54,5 @@ class cloudera::api::managementservice (
     require => [File["CM-roles.json"],Exec["add CM MGMT"]],
     tries   => 3,
     try_sleep => 60
-  }
-
-  exec { "start CM MGMT":
-    command => "/usr/bin/curl -H 'Content-Type: application/json' -u $cm_api_user:$cm_api_password -XPOST \"http://$cm_api_host:$cm_api_port/api/v13/cm/service/commands/start\" && touch /var/tmp/CM-MGMT-started.lock",
-    cwd     => "/tmp",
-    creates => "/var/tmp/CM-MGMT-started.lock",
-    tries   => 3,
-    try_sleep => 60,
-    require => Exec['add role for CM']
   }
 }

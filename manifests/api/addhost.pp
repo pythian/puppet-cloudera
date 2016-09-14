d35e94facadc04296992527b150f4b1965c501a3
Thiago Nache B. de Carvalho <carvalho@pythian.com>
2016-09-14 15:52:45 -0300

    adds rack configuration

--- manifests/api/addhost.pp
+++ manifests/api/addhost.pp
@@ -45,11 +45,26 @@ class cloudera::api::addhost (
     content => template("${module_name}/host.json.erb")
   }
 
-  exec { 'add_host':
-    command => "/usr/bin/curl -H 'Content-Type: application/json' -u $cm_api_user:$cm_api_password -XPOST \"http://$cm_api_host:$cm_api_port/api/v13/clusters/$cdh_cluster_name/hosts\" -d @host.json > $cdh_metadata_dir/$fqdn.json.output",
+  exec { 'add_rack':
+    command => "/usr/bin/curl -H 'Content-Type: application/json' -u $cm_api_user:$cm_api_password -XPOST \"http://$cm_api_host:$cm_api_port/api/v13/hosts\" -d @host.json > $cdh_metadata_dir/host.json.output",
     cwd     => "/tmp",
-    creates => "$cdh_metadata_dir/$fqdn.json.output",
+    creates => "$cdh_metadata_dir/host-cluster.json.output",
     require => [Exec['wait-host-registration'],File['host.json']],
+    tries   => 3,
+    try_sleep => 60
+  }
+
+  file { 'host-cluster.json':
+    ensure  => $file_ensure,
+    path    => '/tmp/host-cluster.json',
+    content => template("${module_name}/host-cluster.json.erb")
+  }
+
+  exec { 'add_host':
+    command => "/usr/bin/curl -H 'Content-Type: application/json' -u $cm_api_user:$cm_api_password -XPOST \"http://$cm_api_host:$cm_api_port/api/v13/clusters/$cdh_cluster_name/hosts\" -d @host-cluster.json > $cdh_metadata_dir/host-cluster.json.output",
+    cwd     => "/tmp",
+    creates => "$cdh_metadata_dir/host-cluster.json.output",
+    require => [Exec['wait-host-registration'],File['host-cluster.json']],
     tries   => 3,
     try_sleep => 60
   }

ffc880fe3c100a20cb1d8dee886d7ae962561e86
Thiago Nache B. de Carvalho <carvalho@pythian.com>
2016-10-13 20:04:52 -0300

    adds FAILOVERCONTROLLER roles

--- manifests/roles/servicenode_2.pp
+++ manifests/roles/servicenode_2.pp
@@ -42,7 +42,7 @@ class cloudera::roles::servicenode_2 (
       }
       cloudera::api::addrole{'MAPREDUCE':
         cdh_cluster_name => $cdh_cluster_name,
-        cdh_service_roles => ['JOBTRACKER'],
+        cdh_service_roles => ['JOBTRACKER','FAILOVERCONTROLLER'],
         cm_api_host => $cm_api_host,
         require => Mount['/nfs/namenode'],
       }
@@ -169,7 +169,7 @@ class cloudera::roles::servicenode_2 (
       }
       cloudera::api::addrole{'MAPREDUCE':
         cdh_cluster_name => $cdh_cluster_name,
-        cdh_service_roles => ['JOBTRACKER'],
+        cdh_service_roles => ['JOBTRACKER','FAILOVERCONTROLLER'],
         cm_api_host => $cm_api_host,
       }
       cloudera::api::addrole{'ZOOKEEPER':

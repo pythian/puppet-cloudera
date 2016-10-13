432af4d82531333ebb1628d9e67d8739f1c8158c
Thiago Nache B. de Carvalho <carvalho@pythian.com>
2016-10-13 18:30:28 -0300

    adds new configuration for mapreduce

--- manifests/roles/server.pp
+++ manifests/roles/server.pp
@@ -73,11 +73,25 @@ class cloudera::roles::server (
     cm_api_host => $cm_api_host,
     require => Class['cloudera::api::addservice[HBASE]']
   }
+  cloudera::api::configservice{'MAPREDUCE':
+    cdh_cluster_name => $cdh_cluster_name,
+    items_config => [{ "name" => "hdfs_service", "value" => "HDFS"},{ "name" => "zookeeper_service", "value" => "ZOOKEEPER"}],
+    cm_api_host => $cm_api_host,
+    require => Class['cloudera::api::addservice[MAPREDUCE]'],
+  }
   cloudera::api::configservice{'HDFS':
     cdh_cluster_name => $cdh_cluster_name,
     items_config => [{ "name" => "zookeeper_service", "value" => "ZOOKEEPER"}],
     cm_api_host => $cm_api_host,
     require => Class['cloudera::api::addrole[HDFS]'],
+  }
+  cloudera::api::configrolegroup{'MAPREDUCE':
+    cdh_cluster_name => $cdh_cluster_name,
+    cdh_cluster_service => 'MAPREDUCE',
+    cdh_service_rolegroup => 'MAPREDUCE-BASE',
+    items_config => [{ "name" => "mapred_local_dir", "value" => "/dfs/mapreduce/jobtracker"}],
+    cm_api_host => $cm_api_host,
+    require => Class['cloudera::api::addservice[YARN]'],
   }
   cloudera::api::configrolegroup{'HDFS-JOURNALNODE-BASE':
     cdh_cluster_name => $cdh_cluster_name,

d8ceb5b1ad9f769079f8af993fac962fbd3d813e
Thiago Nache B. de Carvalho <carvalho@pythian.com>
2016-11-09 11:02:37 -0200

    Revert "remove wrong dependency"
    This reverts commit d5012ed0a73d6749e1e247744cd0846317eef89e.

--- manifests/cluster.pp
+++ manifests/cluster.pp
@@ -81,7 +81,7 @@ class cloudera::cluster (
         } else {
           class { 'mysql::client': }
           mysql_database{ "actmon_db_name": require => Class['::mysql::client'], }
-          mysql_grant{ "$cm_db_user@%/$cm_db_name.*": user => "$cm_db_user@%", table => "$cm_db_name.*", privileges => ['ALL'], require => Class['::mysql::client'], }
+          mysql_grant{ "$cm_db_user@%/$cm_db_name.*": user => "$cm_db_user@%", table => "$cm_db_name.*", privileges => ['ALL'], require => Class["mysql_user[$cm_db_user@%]"], }
           class { '::cloudera':
             cm_server_host => $cm_api_host,
             install_cmserver => true,

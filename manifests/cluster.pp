63786c044146f65b7413575e468e64ff5a73223b
Thiago Nache B. de Carvalho <carvalho@pythian.com>
2016-12-12 13:07:25 -0200

    adds ensure present

--- manifests/cluster.pp
+++ manifests/cluster.pp
@@ -77,9 +77,9 @@ class cloudera::cluster (
           }
         } else {
           class { 'mysql::client': }
-          mysql_database{ "$actmon_db_name": require => Class['::mysql::client'], }
+          mysql_database{ "$actmon_db_name": require => Class['::mysql::client'], ensure => present }
           # this cannot be inside of puppet due to puppetlabs module limitation. it returns exit code non zero because mysqld is not installed. It seems a bug because we can manage pre installed DBs
-          #mysql_user{ "$cm_db_user@%": ensure => present, password_hash => mysql_password("$cm_db_pass"), require => Class['::mysql::client'], }
+          #mysql_user{ "$cm_db_user@%": ensure => present, password_hash => mysql_password("$cm_db_pass"), require => Class['::mysql::client'], ensure => present}
           #mysql_grant{ "$cm_db_user@%/$cm_db_name.*": user => "$cm_db_user@%", table => "$cm_db_name.*", privileges => ['ALL'] }
 
           class { '::cloudera':
@@ -124,7 +124,7 @@ class cloudera::cluster (
           }
         } else {
           class { 'mysql::client': }
-          mysql_database{ "actmon_db_name": require => Class['::mysql::client'], }
+          mysql_database{ "$actmon_db_name": require => Class['::mysql::client'], ensure => present }
           # this cannot be inside of puppet due to puppetlabs module limitation. it returns exit code non zero because mysqld is not installed. It seems a bug because we can manage pre installed DBs
           #mysql_user{ "$cm_db_user@%": ensure => present, password_hash => mysql_password("$cm_db_pass"), require => Class['::mysql::client'], }
           #mysql_grant{ "$cm_db_user@%/$cm_db_name.*": user => "$cm_db_user@%", table => "$cm_db_name.*", privileges => ['ALL'] }

6f7ec3ddf0e6d3c89e8d2cac0c5dc96c99f9965d
Thiago Nache B. de Carvalho <carvalho@pythian.com>
2016-12-14 14:10:31 -0200

    handling parcels download in a better way

--- manifests/parcels/download.pp
+++ manifests/parcels/download.pp
@@ -6,22 +6,40 @@ define cloudera::parcels::download (
   $cm_api_host       = $cloudera::params::cm_api_host,
   $cm_api_port       = $cloudera::params::cm_api_port,
   $cm_api_user       = $cloudera::params::cm_api_user,
-  $cm_api_pass   = $cloudera::params::cm_api_pass
+  $cm_api_pass       = $cloudera::params::cm_api_pass
 ) {
 
-  exec { "parcels-download-$parcels_product":
-    command => "/bin/sleep 3 && /usr/bin/curl -u $cm_api_user:$cm_api_pass -XPOST \"http://$cm_api_host:$cm_api_port/api/v13/clusters/$cdh_cluster_name/parcels/products/$parcels_product/versions/$parcels_version/commands/startDownload\" > $cdh_metadata_dir/parcels-download-$parcels_product.json.output",
-    cwd     => "/tmp",
-    creates => "$cdh_metadata_dir/parcels-download-$parcels_product.json.output",
-    tries   => 3,
+  exec { "parcels-available-remotely-$parcels_product":
+    command   => "/usr/bin/curl -u $cm_api_user:$cm_api_pass -XGET \"http://$cm_api_host:$cm_api_port/api/v13/clusters/$cdh_cluster_name/parcels/products/$parcels_product/versions/$parcels_version\" > $cdh_metadata_dir/parcels-available-remotely-$parcels_product.json.output && grep AVAILABLE_REMOTELY $cdh_metadata_dir/parcels-available-remotely-$parcels_product.json.output",
+    creates   => "$cdh_metadata_dir/parcels-available-remotely-$parcels_product.json.output",
+    tries     => 3,
     try_sleep => 60,
-    notify => Exec["wait-download-complete-$parcels_product"]
+    notify    => Exec["parcels-download-$parcels_product"]
+  }
+
+  exec { "parcels-download-$parcels_product":
+    command     => "/usr/bin/curl -u $cm_api_user:$cm_api_pass -XPOST \"http://$cm_api_host:$cm_api_port/api/v13/clusters/$cdh_cluster_name/parcels/products/$parcels_product/versions/$parcels_version/commands/startDownload\" > $cdh_metadata_dir/parcels-download-$parcels_product.json.output",
+    cwd         => "/tmp",
+    creates     => "$cdh_metadata_dir/parcels-download-$parcels_product.json.output",
+    tries       => 3,
+    try_sleep   => 60,
+    notify      => Exec["parcels-downloading-$parcels_product"],
+    refreshonly => true,
+  }
+
+  exec { "parcels-downloading-$parcels_product":
+    command     => "/usr/bin/curl -u $cm_api_user:$cm_api_pass -XGET \"http://$cm_api_host:$cm_api_port/api/v13/clusters/$cdh_cluster_name/parcels/products/$parcels_product/versions/$parcels_version\" > $cdh_metadata_dir/parcels-downloading-$parcels_product.json.output && grep DOWNLOADING $cdh_metadata_dir/parcels-downloading-$parcels_product.json.output",
+    creates     => "$cdh_metadata_dir/parcels-downloading-$parcels_product.json.output",
+    tries       => 3,
+    try_sleep   => 60,
+    notify      => Exec["wait-download-complete-$parcels_product"],
+    refreshonly => true,
   }
 
   exec { "wait-download-complete-$parcels_product":
-    command => "/usr/bin/curl -u $cm_api_user:$cm_api_pass -XGET \"http://$cm_api_host:$cm_api_port/api/v13/clusters/$cdh_cluster_name/parcels/products/$parcels_product/versions/$parcels_version\" | grep DOWNLOADED",
-    tries => 10,
-    try_sleep => 60,
+    command     => "/usr/bin/curl -u $cm_api_user:$cm_api_pass -XGET \"http://$cm_api_host:$cm_api_port/api/v13/clusters/$cdh_cluster_name/parcels/products/$parcels_product/versions/$parcels_version\" | grep DOWNLOADED",
+    tries       => 10,
+    try_sleep   => 60,
     refreshonly => true,
   }
 }

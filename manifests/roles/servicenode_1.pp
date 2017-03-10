a71dd7c13afa164dc0dfa29073332b8016c35607
Thiago Nache B. de Carvalho <carvalho@pythian.com>
2017-03-10 18:06:32 -0300

    increases wait parcels timeout due to java installation delay

--- manifests/roles/servicenode_1.pp
+++ manifests/roles/servicenode_1.pp
@@ -63,7 +63,7 @@ class cloudera::roles::servicenode_1 (
       }
       exec { 'wait-parcels':
         command => "/usr/bin/curl -u $cm_api_user:$cm_api_pass -XGET \"http://$cm_api_host:$cm_api_port/api/v13/clusters/$cdh_cluster_name/parcels/products/CDH/versions/$cdh_cluster_parcels_release\" | grep ACTIVATED",
-        tries => 15,
+        tries => 45,
         try_sleep => 60,
         require => Class['cloudera::api::addrole[ZOOKEEPER]'],
       }
@@ -115,7 +115,7 @@ class cloudera::roles::servicenode_1 (
       }
       exec { 'wait-parcels':
         command => "/usr/bin/curl -u $cm_api_user:$cm_api_pass -XGET \"http://$cm_api_host:$cm_api_port/api/v13/clusters/$cdh_cluster_name/parcels/products/CDH/versions/$cdh_cluster_parcels_release\" | grep ACTIVATED",
-        tries => 15,
+        tries => 45,
         try_sleep => 60,
         require => Class['cloudera::api::addrole[ZOOKEEPER]'],
       }
@@ -187,7 +187,7 @@ class cloudera::roles::servicenode_1 (
       }
       exec { 'wait-parcels':
         command => "/usr/bin/curl -u $cm_api_user:$cm_api_pass -XGET \"http://$cm_api_host:$cm_api_port/api/v13/clusters/$cdh_cluster_name/parcels/products/CDH/versions/$cdh_cluster_parcels_release\" | grep ACTIVATED",
-        tries => 15,
+        tries => 45,
         try_sleep => 60,
         require => Class['cloudera::api::addrole[ZOOKEEPER]'],
       }
@@ -239,7 +239,7 @@ class cloudera::roles::servicenode_1 (
       }
       exec { 'wait-parcels':
         command => "/usr/bin/curl -u $cm_api_user:$cm_api_pass -XGET \"http://$cm_api_host:$cm_api_port/api/v13/clusters/$cdh_cluster_name/parcels/products/CDH/versions/$cdh_cluster_parcels_release\" | grep ACTIVATED",
-        tries => 15,
+        tries => 45,
         try_sleep => 60,
         require => Class['cloudera::api::addrole[ZOOKEEPER]'],
       }

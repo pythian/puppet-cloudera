def1cd9561f0a60364a2c3753f04747ad9221893
Rodrigo Montoani <rodrigo.montoani-c@mm02sshgtw01.mediamiser.net>
2017-04-13 17:43:54 -0400

    Fixing issues with class on server.pp and cron.pp

--- manifests/cron.pp
+++ manifests/cron.pp
@@ -1,6 +1,9 @@
-cron::hourly { 'mysqlbackup_hourly':
-     minute      => '00',
-     user        => 'root',
-     command     => '/usr/local/bin/mysql_backup_restore.sh backup',
-     description => 'Mysql backup',
+class cloudera::cron ()
+inherits cloudera::params {
+  cron::hourly { 'mysqlbackup_hourly':
+    minute      => '00',
+    user        => 'root',
+    command     => '/usr/local/bin/mysql_backup_restore.sh backup',
+    description => 'Mysql backup',
+  }
 }

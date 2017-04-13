67830a982188fa634bd95931e032e253409e58fb
Rodrigo Montoani <rodrigo.montoani-c@mm02sshgtw01.mediamiser.net>
2017-04-13 14:23:25 -0400

    Add configuration for Backup

---
+++ manifests/cron.pp
@@ -0,0 +1,6 @@
+cron::hourly { 'mysqlbackup_hourly':
+     minute      => '00',
+     user        => 'root',
+     command     => '/usr/local/bin/mysql_backup_restore.sh backup',
+     description => 'Mysql backup',
+}

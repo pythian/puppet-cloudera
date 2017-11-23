6af84e48469fde3fbf357942f454a7cda2b422e5
Thiago Nache B. de Carvalho <carvalho@pythian.com>
2017-11-23 14:01:44 -0200

    removes require for class mysql::server

--- manifests/cluster.pp
+++ manifests/cluster.pp
@@ -61,8 +61,17 @@ class cloudera::cluster (
       } else {
         if $cm_db_rds == 0 {
           if $cm_db_type == "mysql" {
-            class { 'mysql::server': root_password => $cm_db_masterpass, remove_default_accounts => true, require => Service['nfs-kernel-server'], }
-            mysql_user{ "$cm_db_user@%": ensure => present, password_hash => mysql_password("$cm_db_pass"), require => Class['::mysql::server'], }
+            class { 'mysql::server':
+              root_password => $cm_db_masterpass,
+              remove_default_accounts => true,
+              require => Service['nfs-kernel-server'],
+            }
+            mysql_user{ "$cm_db_user@%":
+              ensure => present,
+              password_hash => mysql_password("$cm_db_pass"),
+              # BUG: This class does not exist anymore, but we are using RDS, so will ignore it for now
+              #require => Class['::mysql::server'],
+            }
             mysql_grant{ "$cm_db_user@%/$cm_db_name.*": user => "$cm_db_user@%", table => "$cm_db_name.*", privileges => ['ALL'], require => Class["mysql_user[$cm_db_user@%]"], }
             class { '::cloudera':
               cm_server_host => $cm_api_host,
